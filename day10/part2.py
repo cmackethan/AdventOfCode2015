b9f685ade93f56dde8a83e3eeb9fa4a470256dac
cmackethan <cmackethan@vt.edu>
2024-01-12 18:35:50 -0500

    Day 10

---
+++ day10/part2.py
@@ -0,0 +1,12 @@
+import sys
+import time
+
+def solve(file):
+    return
+
+
+start_time = time.time()
+file = open(sys.argv[1], 'r')
+print(solve(file))
+file.close()
+print("--- %s seconds ---" % (time.time() - start_time))
